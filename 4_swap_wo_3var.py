54ceed233a9083a700bd2eedf9262951cb9bfa72
Shuvrangshu Barua <shuvrangshubarua@gmail.com>
2025-01-13 09:39:24 +0530

    4_swap_wo_3var.py

--- 4_swap_wo_3var.py
+++ 4_swap_wo_3var.py
@@ -2,7 +2,5 @@
 a=int(input("Enter first number : "))
 b=int(input("Enter second number : "))
 print("Before Swap :-- a =",a," b =",b)
-a=a+b
-b=a-b
-a=a-b
+a,b=b,a
 print("After Swap :-- a =",a," b =",b)
